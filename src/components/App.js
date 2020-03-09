5a13e822a0a8e52924561dd245b80baa1272bcee
povilasurbonas <povilas.urbonas85@gmail.com>
2020-03-10 00:50:40 +0100

    adding projects heading

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ class App extends Component {
     return (
       <div>
         <h1>Web Dev Is Fun React App</h1>
+        <p>This is React App</p>
+        <h2>Projects</h2>
       </div>
     );
   }
