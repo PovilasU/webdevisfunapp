dea65d73fbab93d20bc3ae878f9104af2f6d1b24
povilasurbonas <povilas.urbonas85@gmail.com>
2020-03-14 22:56:04 +0100

    Revert "changing bakcround addind css class for gallery"
    This reverts commit d2278f5c513e224484496157e38ff13be754c5dc.

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -18,9 +18,8 @@ import Makemake from "../img/planets/Makemake_dwarf_planet.jpg";
 import Eris from "../img/planets/eris_dwarf_planet.jpeg";
 
 export default Gallery => {
-  document.body.style.backgroundColor = "#000000";
   return (
-    <div className="gallery-planets">
+    <div>
       <h1>Solar System</h1>
       <h2></h2>
       <p>Technologies used:</p>
